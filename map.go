f08577fbdce9c5be98718a2acaaedd625e1fa49d
Mina Naguib <mina.git@naguib.ca>
2013-09-13 20:32:12 -0400

    Ownership helper methods

--- map.go
+++ map.go
@@ -8,9 +8,14 @@ type NodeType struct {
 
 // Map represents the map and the nodes in it
 type Map struct {
-	Game             *Game
-	NodeTypes        map[string]*NodeType
-	Nodes            map[int]*Node
+	Game      *Game
+	Directed  bool
+	NodeTypes map[string]*NodeType
+	Nodes     map[int]*Node
+	// Lazy caches
+	free_nodes       []*Node
+	owned_nodes      []*Node
+	enemy_nodes      []*Node
 	controlled_nodes []*Node
 }
 
@@ -20,6 +25,7 @@ func NewMap(game *Game) (m *Map, err error) {
 
 	m = &Map{
 		Game:      game,
+		Directed:  request.Infos.Directed,
 		NodeTypes: make(map[string]*NodeType),
 		Nodes:     make(map[int]*Node),
 	}
@@ -48,7 +54,7 @@ func NewMap(game *Game) (m *Map, err error) {
 
 	for _, rp := range request.Map.Paths {
 		m.Nodes[rp.From].link_to(m.Nodes[rp.To])
-		if request.Infos.Directed == false {
+		if m.Directed == false {
 			m.Nodes[rp.To].link_to(m.Nodes[rp.From])
 		}
 	}
@@ -56,13 +62,53 @@ func NewMap(game *Game) (m *Map, err error) {
 	return m, nil
 }
 
+func (m *Map) FreeNodes() []*Node {
+	if m.free_nodes != nil {
+		return m.free_nodes
+	}
+	m.free_nodes = make([]*Node, 0, len(m.Nodes)/2)
+	for _, node := range m.Nodes {
+		if node.IsFree() {
+			m.free_nodes = append(m.free_nodes, node)
+		}
+	}
+	return m.free_nodes
+}
+
+func (m *Map) OwnedNodes() []*Node {
+	if m.owned_nodes != nil {
+		return m.owned_nodes
+	}
+	m.owned_nodes = make([]*Node, 0, len(m.Nodes)/2)
+	for _, node := range m.Nodes {
+		if node.IsOwned() {
+			m.owned_nodes = append(m.owned_nodes, node)
+		}
+	}
+	return m.owned_nodes
+}
+
+func (m *Map) EnemyNodes() []*Node {
+	if m.enemy_nodes != nil {
+		return m.enemy_nodes
+	}
+	m.enemy_nodes = make([]*Node, 0, len(m.Nodes)/2)
+	for _, node := range m.Nodes {
+		if node.IsEnemy() {
+			m.enemy_nodes = append(m.enemy_nodes, node)
+		}
+	}
+	return m.enemy_nodes
+}
+
 func (m *Map) ControlledNodes() []*Node {
-	if m.controlled_nodes == nil {
-		m.controlled_nodes = make([]*Node, 0, len(m.Nodes)/2)
-		for _, node := range m.Nodes {
-			if node.Player_Id == m.Game.Player_Id {
-				m.controlled_nodes = append(m.controlled_nodes, node)
-			}
+	if m.controlled_nodes != nil {
+		return m.controlled_nodes
+	}
+	m.controlled_nodes = make([]*Node, 0, len(m.Nodes)/2)
+	for _, node := range m.Nodes {
+		if node.IsControlled() {
+			m.controlled_nodes = append(m.controlled_nodes, node)
 		}
 	}
 	return m.controlled_nodes
--- node.go
+++ node.go
@@ -27,15 +27,36 @@ func (node *Node) link_to(other *Node) {
 	other.Paths_Inbound[node.Id] = node
 }
 
-func (node *Node) IsOwned() bool {
-	return node.Player_Id == node.Map.Game.Player_Id
+// IsFree returns whether the node is free, or owned by any player
+func (node *Node) IsFree() bool {
+	return node.Player_Id < 0
 }
+
+// IsOwned returns whether this node is owned by the current player
+//
+// Note - this deviates from the ruby client implementation, where ruby's owned? is essentially the opposite of free? - this is quite confusing as naturally asking a node.IsOwned() most likely indicates the caller wants to know if they own it themselves
+//
+// Callers who wish to mimick the owned? behavior of the ruby client may simply ask for !node.IsFree()
+func (node *Node) IsOwned() bool {
+	return node.IsOwnedBy(node.Map.Game.Player_Id)
+}
+
+func (node *Node) IsOwnedBy(player_id int) bool {
+	return node.Player_Id == player_id
+}
+
+func (node *Node) IsEnemy() bool {
+	return !node.IsFree() && !node.IsOwned()
+}
+
+func (node *Node) IsControlled() bool {
+	return node.IsOwned() && node.Number_Of_Soldiers > 0
+}
+
 func (node *Node) reset() {
+	node.Incoming_Soldiers = 0
+	node.Available_Soldiers = 0
 	if node.IsOwned() {
 		node.Available_Soldiers = node.Number_Of_Soldiers
-		node.Incoming_Soldiers = 0
-	} else {
-		node.Available_Soldiers = 0
-		node.Incoming_Soldiers = 0
 	}
 }
